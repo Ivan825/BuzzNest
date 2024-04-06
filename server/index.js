8c25b426d5fdae1420cfa2add33fbf1f17c7d90f
NirbhaySharma504 <Nirbhay.Sharma@iiitb.ac.in>
2024-04-06 18:49:45 +0530

    auth and user account

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ import path from "path";
 //securty packges
 import helmet from "helmet";
 import dbConnection from "./dbConfig/index.js";
+import errorMiddleware from "./middleware/errorMiddleware.js";
+import router from "./routes/index.js";
 
+const __dirname = path.resolve(path.dirname(""));
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "views/build")));
 
 const PORT = process.env.PORT || 8800;
 
@@ -26,8 +30,10 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("dev"));
+app.use(router);
 
 //error middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
