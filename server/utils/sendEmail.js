74c77521534e8f987f420e0e2bfa449749fb24b3
Ivan825 <65112113+Ivan825@users.noreply.github.com>
2024-04-08 20:20:44 +0530

    Update sendEmail.js

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -92,7 +92,7 @@ export const resetPasswordLink = async (user, res) => {
   const { _id, email } = user;
 
   const token = _id + uuidv4();
-  const link = "https://buzznest-3.onrender.com" + "users/reset-password/" + _id + "/" + token;
+  const link = "https://buzznest-3.onrender.com/" + "users/reset-password/" + _id + "/" + token;
 
   //   mail options
   const mailOptions = {
